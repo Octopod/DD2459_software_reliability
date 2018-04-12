b5073ff6321663dfb21e751570550432ae4e683d
hanna <hnyblom@kth.se>
2018-04-12 21:39:29 +0200

    third

--- src/se/denisen/testing.java
+++ src/se/denisen/testing.java
@@ -9,6 +9,7 @@ import org.junit.runners.Parameterized;
 import java.io.*;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Random;
 
 class testing {
@@ -48,15 +49,17 @@ class testing {
        String[] splitStr;
        int[] testArr;
        testArr = new int[10];
+       HashMap newmap = new HashMap();
        while((line=bufferedReader.readLine())!=null){
            splitStr = line.trim().split("\\s+");
            for(int i=0; i<splitStr.length-1;i++){
                int somet =Integer.parseInt(splitStr[i]);
                testArr[i] = somet;
+               newmap.put(i,somet);
            }
            int last = Integer.parseInt(splitStr[10]);
            int ret = Main.start(testArr, last);
-           if(Arrays.asList(testArr).contains(last)){
+           if(newmap.containsValue(last)){
                assertEquals(ret,last);
            }else{
                assertEquals(ret, -1);
