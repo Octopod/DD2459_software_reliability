6f23a71f53d23f69d27d4135f2662e9acc7a27e0
Octopod <denise.nordlof@gmail.com>
2018-04-12 14:52:38 +0200

    init

---
+++ src/se/denisen/testing.java
@@ -0,0 +1,19 @@
+package se.denisen;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class testing {
+
+    @Test
+    void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    void testHello() {
+        assertEquals(2, 3);
+    }
+
+}
