ed88f5469fb97f726e51f29ab7c999007bcb5297
hanna <hnyblom@kth.se>
2018-05-10 10:56:53 +0200

    Fifth err

--- src/se/denisen/Main.java
+++ src/se/denisen/Main.java
@@ -40,7 +40,8 @@ public class Main {
             return -1;
         } else if (L[mid] == k) {
             return L[mid];
-        } else if (L[mid] < k) {
+            //Err5: corr: L[mid]<k
+        } else if (L[mid] > k) {
             return rBsearch(L, mid + 1, high, k);
         } else {
             return rBsearch(L, low, mid - 1, k);
