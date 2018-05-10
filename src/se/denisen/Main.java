97795138756a28a415ce60d0e3069e2f64043acf
hanna <hnyblom@kth.se>
2018-05-10 10:47:26 +0200

    Second err

--- src/se/denisen/Main.java
+++ src/se/denisen/Main.java
@@ -72,7 +72,8 @@ public class Main {
                 fos, "UTF-8"));
         try {
             for (int i = 0; i < 10; i++) {
-                for(int j=0; j<11; j++){
+                //Err: corr: j<11
+                for(int j=0; j<10; j++){
                     out.write(Integer.toString(rand.nextInt(9))+ " ");
 
                 }
